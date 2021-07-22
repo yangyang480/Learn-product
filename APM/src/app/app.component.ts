3d92de9e6f258c6d97ba8f6c62ed68fcb448c78a
yangyang480 <82347891+yangyang480@users.noreply.github.com>
2021-07-21 19:05:28 -0500

    Get to good current state.
    Fix filter and show things.

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from "@angular/core"
 
 @Component({
     selector: 'pm-root',
-    template: `<div><h1>{{pageTitle}}</h1>
-    <pm-products></pm-products>
-  </div>
+    template:`
+    <nav class='navbar-expand navbar-light bg-light'>
+      <a class='navbar-brand'>{{pageTitle}}</a>
+      <ul class= 'nav nav-pills'>
+        <li><a class='nav-link' routerLink='/welcome'>Home</a></li>
+        <li><a class='nav-link' routerLink='/products'>Product List</a></li>
+      </ul>
+    </nav>
+    <div class='contain'>
+      <router-outlet></router-outlet>
+    </div>
   `
 })
 export class AppComponent {
--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
 import { Year2021Component } from './Yangyang/Year-2021.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
+    WelcomeComponent,
     Year2021Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    HttpClientModule,
+    RouterModule. forRoot([
+      { path:'welcome', component: WelcomeComponent },
+      { path:'', redirectTo: 'welcome', pathMatch: 'full' },
+      { path:'***', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
